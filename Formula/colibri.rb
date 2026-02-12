d35eb3ba83d286d8261a3bb8852dcaf6c41e2f29
TobiSchelling <tobias.schelling@icloud.com>
2026-02-12 22:15:39 +0100

    Update colibri to v0.6.1

--- Formula/colibri.rb
+++ Formula/colibri.rb
@@ -1,13 +1,13 @@
 class Colibri < Formula
   desc "Local RAG system for semantic search over markdown content"
   homepage "https://github.com/TobiSchelling/CoLibri"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/TobiSchelling/CoLibri/releases/download/v#{version}/colibri-#{version}-macos-arm64.tar.gz"
-      sha256 "a473ae0e47fe08bbc8c638d90b9a894923f86797c259d345bbea4f662a9e7454"
+      sha256 "442d41cf009cca3bd8c9293455d522953e16153f1db35d60e3cfbbc0a56b9488"
     end
   end
 
