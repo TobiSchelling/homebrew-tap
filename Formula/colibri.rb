89f321c528af2c22487df7c300638aa5a5e7dbf5
TobiSchelling <tobias.schelling@icloud.com>
2026-02-22 07:16:23 +0100

    chore(colibri): bump to v0.8.2

--- Formula/colibri.rb
+++ Formula/colibri.rb
@@ -1,13 +1,13 @@
 class Colibri < Formula
   desc "Local RAG system for semantic search over markdown content"
   homepage "https://github.com/TobiSchelling/CoLibri"
-  version "0.8.1"
+  version "0.8.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/TobiSchelling/CoLibri/releases/download/v#{version}/colibri-#{version}-macos-arm64.tar.gz"
-      sha256 "0e2653de6a21353f64ff2d10de98385d51d730e8e80887cde91fed2d5d855c62"
+      sha256 "067f5969760b8d28ceb4afde0f5f2a1b2ae5af431c80899e4bd93209d2cd8fec"
     end
   end
 
